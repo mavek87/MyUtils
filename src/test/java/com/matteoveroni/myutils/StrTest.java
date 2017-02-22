574b2ddf112c5956e9e01afe7030cf2321a9b362
Matteo Veroni <matver87@gmail.com>
2017-02-22 02:49:10 +0100

    tested Str.generateRndString(Range range) method

--- src/test/java/com/matteoveroni/myutils/StrTest.java
+++ src/test/java/com/matteoveroni/myutils/StrTest.java
@@ -1,5 +1,6 @@
 package com.matteoveroni.myutils;
 
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
@@ -7,12 +8,33 @@ import org.junit.Test;
  * @author Matteo Veroni
  */
 public class StrTest {
-
-    public StrTest() {
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void test_generateRndString_UsingLowRangeBound_GreaterThan_HiRangeBound_ThrowsIllegalArgumentException() {
+        int lowBound = 2;
+        int hiBound = 1;
+        
+        Range range = new Range(lowBound, hiBound);
+        Str.generateRndString(range);
     }
-
-    @Test
-    public void generateRndString_UsingLowRangeBoundSmallerThanHiRangeBound_ThrowsIllegalArgumentException() {
-
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void test_generateRndString_UsingLowRangeBound_SmallerThan_HiRangeBound_ThrowsIllegalArgumentException() {
+        int hiBound = 2;
+        int lowBound = hiBound;
+        
+        Range range = new Range(lowBound, hiBound);
+        Str.generateRndString(range);
     }
+    
+    public void test_generateRndString_UsingLowRangeBound_SmallerThan_HiRangeBound_ReturnsRandomString() {
+        int lowBound = 1;
+        int hiBound = 2;
+        
+        Range range = new Range(lowBound, hiBound);
+        String generatedString = Str.generateRndString(range);
+        
+        assertTrue(generatedString.length() > 0);
+    }
+    
 }

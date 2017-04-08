5d0df89a8a19e8cb9d732cfd8412818d20cd09ed
Matteo Veroni <matver87@gmail.com>
2017-04-08 23:00:01 +0200

    Created Int class and getRandomInteger (min, max) method

---
+++ src/main/java/com/matteoveroni/myutils/Int.java
@@ -0,0 +1,22 @@
+package com.matteoveroni.myutils;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+/**
+ *
+ * @author Matteo Veroni
+ */
+public class Int {
+
+    private static final Random RAND_GENERATOR = new SecureRandom();
+    
+    public static int getRandomInteger(int min, int max) throws IllegalArgumentException {
+        if (min < 0 || max < 0 || min >= max) {
+            throw new IllegalArgumentException();
+        }
+                
+        return RAND_GENERATOR.nextInt((max - min) + 1) + min;
+    }
+
+}

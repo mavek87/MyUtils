891524c98370d49456679b4d13658dbc9d62d6f7
veroni <matver87@gmail.com>
2017-04-11 14:49:21 +0200

    renamed Version class into MyUtilsVersion

---
+++ src/main/java/com/matteoveroni/myutils/MyUtilsVersion.java
@@ -0,0 +1,13 @@
+package com.matteoveroni.myutils;
+
+/**
+ *
+ * @author Matteo veroni
+ * 
+ * @version 1.7
+ * 
+ */
+public class MyUtilsVersion {
+
+    public static final String NUMBER = "1.7";
+}

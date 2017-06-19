b93abe68a92fa2758bf8a0d6ce86af11de15732c
Matteo Veroni <matver87@gmail.com>
2017-06-19 15:56:08 +0200

    implemented concatenation method for formattedStrings

--- src/main/java/com/matteoveroni/myutils/FormattedString.java
+++ src/main/java/com/matteoveroni/myutils/FormattedString.java
@@ -1,5 +1,7 @@
 package com.matteoveroni.myutils;
 
+import java.lang.reflect.Array;
+
 /**
  * @author Matteo Veroni
  */
@@ -23,5 +25,22 @@ public class FormattedString {
 
     public Object[] getArgs() {
         return args;
+    }
+
+    public FormattedString concat(FormattedString formattedStringToConcatenate) {
+        String concatFormattedString = formattedString + formattedStringToConcatenate.getFormattedString();
+        Object[] concatArgs = concatenate(args, formattedStringToConcatenate.getArgs());
+        return new FormattedString(concatFormattedString, concatArgs);
+    }
+
+    private <T> T[] concatenate(T[] a, T[] b) {
+        int aLen = a.length;
+        int bLen = b.length;
+
+        T[] c = (T[]) Array.newInstance(a.getClass().getComponentType(), aLen + bLen);
+        System.arraycopy(a, 0, c, 0, aLen);
+        System.arraycopy(b, 0, c, aLen, bLen);
+
+        return c;
     }
 }

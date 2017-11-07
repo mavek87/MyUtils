f0de2fa1031f347f61266358922f344e38516854
Matteo Veroni <matver87@gmail.com>
2017-11-07 17:16:42 +0100

    changed version

--- src/main/java/com/matteoveroni/myutils/MyUtilsVersion.java
+++ src/main/java/com/matteoveroni/myutils/MyUtilsVersion.java
@@ -4,10 +4,10 @@ package com.matteoveroni.myutils;
  *
  * @author Matteo veroni
  * 
- * @version 1.9
+ * @version 2.0
  * 
  */
 public class MyUtilsVersion {
 
-    public static final String NUMBER = "1.9";
+    public static final String NUMBER = "2.0";
 }
